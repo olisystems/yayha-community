2d44da7904acb45f880444f5464c18fa439ba207
m-yahya <yahya_gis@hotmail.com>
2020-02-04 17:05:18 +0100

    Update app styles

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <DoseCommunity />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DoseCommunity from "./components/DoseCommunity.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    DoseCommunity
   }
 };
 </script>
 
 <style>
+body {
+  background-color: #f1eded;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
